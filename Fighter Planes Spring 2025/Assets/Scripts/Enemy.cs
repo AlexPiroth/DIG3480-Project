964259138ed601aac1e712443176cc604d141e41
AlexPiroth <alexpiroth@gmail.com>
2025-04-02 12:01:23 -0400

    Debugging
    Debugged code for Task 2 and some errors from Github

--- Fighter Planes Spring 2025/Assets/Scripts/Enemy.cs
+++ Fighter Planes Spring 2025/Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,33 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private float enemy2Horizontal;
+    [SerializeField] private int enemyType;
+    private float horizontalLimit = 8.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemy2Horizontal = Random.Range(-2f, 2f);
+        enemy2Horizontal = UnityEngine.Random.Range(-2f, 2f);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
 
-        transform.Translate(new Vector3(enemy2Horizontal, -1, 0) * Time.deltaTime * 2f);
+        if (enemyType == 1)
+        {
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+        }
+        else if (enemyType == 2)
+        {
+            transform.Translate(new Vector3(enemy2Horizontal, -1, 0) * Time.deltaTime * 2f);
+            if (Math.Abs(transform.position.x) >= horizontalLimit)
+            {
+                enemy2Horizontal = -enemy2Horizontal;
+            }
+        }
         if (transform.position.y < -6.5)
         {
             Destroy(this.gameObject);
--- Fighter Planes Spring 2025/Assets/Scripts/GameManager.cs
+++ Fighter Planes Spring 2025/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject EnemyOnePrefab;
+    public GameObject EnemyTwoPrefab;
     // Start is called before the first frame update
     void Start()
     {
