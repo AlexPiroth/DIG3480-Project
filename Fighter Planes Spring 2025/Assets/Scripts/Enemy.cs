563b9a734e9a62279a349fb62f69ab080d498c02
BrandonChristensen2 <brandonmakesgames5@gmail.com>
2025-04-01 22:21:12 -0400

    Update Enemy.cs
    After testing, it makes some enemies go in random directions.

--- Fighter Planes Spring 2025/Assets/Scripts/Enemy.cs
+++ Fighter Planes Spring 2025/Assets/Scripts/Enemy.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private float enemy2Horizontal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy2Horizontal = Random.Range(-2f, 2f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
         transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+
+        transform.Translate(new Vector3(enemy2Horizontal, -1, 0) * Time.deltaTime * 2f);
         if (transform.position.y < -6.5)
         {
             Destroy(this.gameObject);
         }
     }
 }
+
