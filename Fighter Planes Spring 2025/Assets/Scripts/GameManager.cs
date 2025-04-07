f04b3ee025a2f0264c87f4d957c3ede863f88163
BrandonChristensen2 <brandonmakesgames5@gmail.com>
2025-04-06 23:20:39 -0400

    Updated GameManager
    I added Coin spawning, pausing for 5s for player to get coin, and added CoinUpdate function to destroy coin and add score/don't add score.

--- Fighter Planes Spring 2025/Assets/Scripts/GameManager.cs
+++ Fighter Planes Spring 2025/Assets/Scripts/GameManager.cs
@@ -1,30 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject EnemyOnePrefab;
+
+    public GameObject playerPrefab;
+    public GameObject enemyOnePrefab;
+    public GameObject cloudPrefab;
+
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI scoreText;
+
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    public int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CreateEnemyOne", 1, 2);
-        InvokeRepeating("CreateEnemyTwo", 6, Random.Range(1f, 5f));
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+        score = 0;
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
+        InvokeRepeating("CreateEnemy", 1, 3);
+        AddScore(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(playerIsAlive)
+        {
+            //make sure coin isn't on screen
+            if(Coin == null)
+            {
+                SpawnObject();
+                //Pause for 6s
+                WaitForSeconds(5f);
+                CoinUpdate();
+            }          
+        }
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
         
     }
-
-    void CreateEnemyOne()
+    public void AddScore(int earnedScore)
     {
-        Instantiate(EnemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+        score = score + earnedScore;
+        scoreText.text = "Score: " + score;
     }
 
-    void CreateEnemyTwo()
+    public void ChangeLivesText (int currentLives)
     {
-        Instantiate(EnemyTwoPrefab, new Vector3(Random.Range(-7f, 7f), 6.5f, 0), Quaternion.identity);
+        livesText.text = "Lives: " + currentLives;
+    }
+
+
+    void SpawnObject()
+        {
+            //Randomizes position
+            Vector3 randomPosition = new Vector3(Random.Range(0, 10f), 0, Random.Range(0, 6.5f));
+            //Create Coin
+            Instantiate(Coin, randomPosition, Quaternion.identity);
+        }
+
+    void CoinUpdate()
+    {
+        if(collision.gameObject.CompareTag("Coin"))
+        {
+            //Calls AddScore Function to add 1 to score
+            AddScore(1);
+        }
+        //Destroy Coin
+        Destroy(Coin);
     }
 }
